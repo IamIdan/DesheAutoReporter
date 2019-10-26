45718ccf31cf261f63d3b55a54312dc8228b82a3
doom3007 <doom3007@gmail.com>
2019-10-26 12:20:44 +0300

    GUI updater and more globalizing made

--- MySalary/MySalary.py
+++ MySalary/MySalary.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, Button, Menu, Entry, Label, Checkbutton
-
+from threading import Thread
+from time import sleep
 
 class ExcelAnalyzer:
     def __init__(self, path):
@@ -60,44 +61,52 @@ class ViewManager:
         # self.network_manager = SeleniumManager()
         # self.dater = DateManager()
         self.labels, self.entries, self.buttons = list(), list(), list()
+        self.root = Tk()
         self.tk_root_setup()
 
     def tk_root_setup(self):
-        root = Tk()
-        root.title("Deshe AutoReporter")
-        self.labels.append(Label(master=root, text="Username: "))
-        self.labels.append(Label(master=root, text="Password: "))
+        self.root.title("Deshe AutoReporter")
+        self.labels.append(Label(master=self.root, text="Username: "))
+        self.labels.append(Label(master=self.root, text="Password: "))
         for i in range(len(self.labels)):
             self.labels[i].grid(row=i, column=0)
-        self.entries.append(Entry(master=root, text="Username", command=None))
-        self.entries.append(Entry(master=root, text="Password", show="*", command=None))
+        self.entries.append(Entry(master=self.root, text="Username", command=None))
+        self.entries.append(Entry(master=self.root, text="Password", show="*", command=None))
         for i in range(len(self.entries)):
             self.entries[i].grid(row=i, column=1)
-        self.buttons.append(Button(master=root, text="Login", command=self.login))
-        self.buttons.append(Checkbutton(master=root, text="Override existing data"))
+        self.buttons.append(Button(master=self.root, text="Login", command=self.login))
+        self.buttons.append(Checkbutton(master=self.root, text="Override existing data"))
         self.buttons.append(
-            Button(master=root, text="Submit Hours from Excel", command=None, state='disabled'))
+            Button(master=self.root, text="Submit Hours from Excel", command=None, state='disabled'))
         for entry in self.entries:
             entry.bind('<Return>', self.login)
         for i in range(len(self.buttons)):
             self.buttons[i].grid(row=2, column=i)
-        menubar = Menu(master=root)
-        root.config(menu=menubar)
-        menubar.add_command(label="Close Window", command=root.quit)
-        root.after(200, lambda: self.geometry_setter(root))
-        root.mainloop()
+        menubar = Menu(master=self.root)
+        self.root.config(menu=menubar)
+        menubar.add_command(label="Close Window", command=self.root.quit)
+        self.root.after(200, self.geometry_setter)
+        Thread(target=self.updater).start()
+        self.root.mainloop()
 
-    def state_setter(self):
-        if self.network_manager.is_logged_in:
-            self.buttons['submitexcelhours']['state'] = 'normal'
-            self.buttons['authentication']['text'] = 'logout'
-            for entry in self.entries:
-                entry['state']['state'] = 'disabled'
-        else:
-            self.buttons['submitexcelhours']['state'] = 'disabled'
-            self.buttons['authentication']['text'] = 'login'
-            for entry in self.entries:
-                entry['state']['state'] = 'normal'
+    def updater(self):
+        while True:
+            sleep(1)
+            # if self.network_manager.is_logged_in:
+            #     self.update_when_need(self.buttons['submitexcelhours'], 'state', 'normal')
+            #     self.update_when_need(self.buttons['authentication'], 'text', 'logout')
+            #     for entry in self.entries:
+            #         self.update_when_need(entry, 'state', 'disabled')
+            # else:
+            #     self.update_when_need(self.buttons['submitexcelhours'], 'state', 'disabled')
+            #     self.update_when_need(self.buttons['authentication'], 'text', 'login')
+            #     for entry in self.entries:
+            #         self.update_when_need(entry, 'state', 'normal')
+
+    @staticmethod
+    def update_when_need(element, field_to_change, variable):
+        if element[field_to_change] != variable:
+            element[field_to_change] = variable
 
     def login(self, event=None):
         # self.network_manager.login(url (const), credentials (username with password credentials))
@@ -116,17 +125,15 @@ class ViewManager:
         #     self.network_manager.report_shift(date['start_date'], date['end_date'])
         pass
 
-    @staticmethod
-    def geometry_setter(root):
-        root.minsize(width=root.winfo_width() + 150,
-                     height=root.winfo_height())
-        x_width_pos = int((root.winfo_screenwidth() - root.winfo_width()) / 2)
+    def geometry_setter(self):
+        self.root.minsize(width=self.root.winfo_width() + 150, height=self.root.winfo_height())
+        x_width_pos = int((self.root.winfo_screenwidth() - self.root.winfo_width()) / 2)
         y_height_pos = int(
-            (root.winfo_screenheight() - root.winfo_height()) / 2)
-        geometry_text = "" + str(root.winfo_width() + 150) + "x" + str(root.winfo_height()) + "+" + str(
+            (self.root.winfo_screenheight() - self.root.winfo_height()) / 2)
+        geometry_text = "" + str(self.root.winfo_width() + 150) + "x" + str(self.root.winfo_height()) + "+" + str(
             x_width_pos - 75) + "+" + str(y_height_pos) + ""
-        root.geometry(geometry_text)
-        root.update()
+        self.root.geometry(geometry_text)
+        self.root.update()
 
 
 if __name__ == '__main__':
